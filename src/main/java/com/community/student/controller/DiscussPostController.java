9c74a19fb5d36667b0f3dee4422ffe7fae1ec2ea
houyuyu <792527365@qq.com>
2019-10-14 17:59:43 +0800

    add cache

--- src/main/java/com/community/student/controller/DiscussPostController.java
+++ src/main/java/com/community/student/controller/DiscussPostController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.*;
 
+
 @Controller
 @RequestMapping("/discuss")
 public class DiscussPostController implements CommunityConstant {
